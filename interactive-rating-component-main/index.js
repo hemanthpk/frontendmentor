fac919cbed4f04fd77210b630010319b2e295798
hemanthpk <141862162+hemanthpk@users.noreply.github.com>
2023-10-15 21:54:47 +0530

    Interactive Rating Project created

---
+++ interactive-rating-component-main/index.js
@@ -0,0 +1,7 @@
+const rs = document.querySelector("#ratingState")
+const ts = document.querySelector("#thankYouState")
+
+function submitFunction() {
+    rs.style.display = "none";
+    ts.style.display = "block";
+}
