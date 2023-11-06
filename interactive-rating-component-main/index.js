2f9762a770c3e3a51a2623a30884ad4fe5ee416f
hemanthpk <141862162+hemanthpk@users.noreply.github.com>
2023-11-06 14:20:12 +0530

    interactive-rating final

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,20 +1,27 @@
 const ratingstate = document.querySelector("#ratingState")
 const thankyoustate = document.querySelector("#thankYouState")
+const buttonControl = document.getElementById("submitButton")
+const ele = document.getElementsByClassName("rating-value")
+
+function checkRadio() {
+	for(i=0;i<ele.length;i++){
+		if(ele[i].checked)	{
+			buttonControl.disabled = false;
+		}
+	}
+}
+
+for(i=0;i<ele.length;i++)
+	ele[i].addEventListener("change",checkRadio)
 
 function submitFunction() {
-	var ele = document.getElementsByName("rating")
 	x = true;
 	for (i=0;i<ele.length;i++)  {
 		if(ele[i].checked) {   
 			ratingstate.style.display = "none"
 			thankyoustate.style.display = "flex"
 			document.getElementById("givenrating").innerText=ele[i].value
-			x = false
 			break
 		}
-	}
-	if(x)
-	{
-		window.alert("Please give rating")
 	}
 }
